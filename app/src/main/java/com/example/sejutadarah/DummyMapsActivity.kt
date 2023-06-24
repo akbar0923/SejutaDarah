7313e9f6c62b860fa2d8c11aab63185a46b43883
ochi <syauqialfeth@gmail.com>
2023-06-24 08:59:21 +0800

    perbaikan xml donor connect fragment , Tambah riwayat_donor_fragment, riwayat_view, riwayatAdapter

--- app/src/main/java/com/example/sejutadarah/DummyMapsActivity.kt
+++ app/src/main/java/com/example/sejutadarah/DummyMapsActivity.kt
@@ -38,6 +38,7 @@ class DummyMapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
         binding = ActivityDummyMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
