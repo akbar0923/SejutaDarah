95b21421fd550e2fed0611c11ef0f3c05bf29d71
Muhammad Akbar <akbar0923>
2023-07-11 00:05:28 +0800

    tambah riwayat donor fragment, adapter, sambung firebase, riwayatDonor class

---
+++ app/src/main/java/com/example/sejutadarah/Database/riwayatDonor.kt
@@ -0,0 +1,38 @@
+package com.example.sejutadarah.Database
+
+import com.google.firebase.firestore.PropertyName
+
+class riwayatDonor (
+    @get:PropertyName("beratBadan")
+    @set:PropertyName("beratBadan")
+    var beratBadan: String = "",
+
+    @get:PropertyName("denyutNadi")
+    @set:PropertyName("denyutNadi")
+    var denyutNadi: String = "",
+
+    @get:PropertyName("golonganDarah")
+    @set:PropertyName("golonganDarah")
+    var golonganDarah: String = "",
+
+    @get:PropertyName("lokasiDonor")
+    @set:PropertyName("lokasiDonor")
+    var lokasiDonor: String = "",
+
+    @get:PropertyName("suhuBadan")
+    @set:PropertyName("suhuBadan")
+    var suhuBadan: String = "",
+
+    @get:PropertyName("tanggalDonor")
+    @set:PropertyName("tanggalDonor")
+    var tanggalDonor: String = "",
+
+    @get:PropertyName("tekananDarah")
+    @set:PropertyName("tekananDarah")
+    var tekananDarah: String = "",
+
+    @get:PropertyName("tinggiBadan")
+    @set:PropertyName("tinggiBadan")
+    var tinggiBadan: String = ""
+
+)
